c71b7df45ebfb673ddf6736150450f5fedd8c9c4
hyquaq <hyquaq@gmail.com>
2020-10-23 20:22:07 +0700

    case division zero is true

---
+++ caseDivisionZero-True.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    int a = 0;
+    if(1.0/9 || 1==1){
+        printf("selection true");
+    }
+    else{
+        printf("selection false");
+    }
+    return 0;
+}
